474cba6ce8e4404cb7fbc1b1e9d4694ac2676890
vivek007-dotcom <vivek.sharma@signitysolutions.in>
2025-11-13 16:51:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -225,3 +225,6 @@ def patient_intake():
         logger.exception("File write failed")
 
     pad_result = {}
+if __name__ == "__main__":
+    logger.info("Server started on http://127.0.0.1:3000")
+    app.run(host="127.0.0.1", port=3000, debug=False)
